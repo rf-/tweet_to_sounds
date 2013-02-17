c849e318d90530f55a74072e51db107a69024d31
Devin Riley <devin@Devins-MacBook-Air.local>
2013-02-16 23:50:34 -0800

    add get_volumes method

--- lib/tweet_to_sounds/volume_getter.rb
+++ lib/tweet_to_sounds/volume_getter.rb
@@ -5,6 +5,15 @@ module TweetToSounds
     end
 
     def get_volumes
+      volumes = []
+      key = TweetToSounds.freesound_api_key
+      threads = @sounds.map do |sound|
+        Thread.new(sound) do |s|
+          Thread.current[:results] = \
+            Net::HTTP.get(URI("http://www.freesound.org/api/sounds/#{sound.id}/analysis/lowlevel/average_loudness/?api_key=#{key}")).to_f.round(2)
+        end 
+      end
+      volumes = threads.flat_map { |t| t.join; t[:results] }
     end
   end
 end
